5434ea9984d0ac8034432395326084ab1bda944f
jigi <ahraeha@naver.com>
2023-01-13 22:02:19 +0900

    "23-01-13" - 계좌확인 API 작업완료 / 테스트 케이스 실행

--- src/main/java/com/example/account/repository/AccountRepository.java
+++ src/main/java/com/example/account/repository/AccountRepository.java
@@ -15,4 +15,5 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     Integer countByAccountUser(AccountUser accountUser);
 
     Optional<Account> findByAccountNumber(String AccountNumber);
+    List<Account> findByAccountUser(AccountUser accountUser);
 }
